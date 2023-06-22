980d1a81d0f467d44d0fff8ac1e509407cb019a5
Nahid1911 <nahid.raihan@gmail.com>
2023-06-22 21:04:08 +0600

    Make some changes in styling

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaRegUser } from 'react-icons/fa6';
 
 const NavBar = () => (
   <nav className="navBar">
     <h1>Bookstore CMS</h1>
     <ul className="navUl">
       <li className="naveLi">
-        <Link to="/">Books</Link>
+        <Link to="/">BOOKS</Link>
       </li>
       <li className="naveLi">
-        <Link to="/categories">Categories</Link>
+        <Link to="/categories">CATEGORIES</Link>
       </li>
     </ul>
+    <button className="icon-button" type="button">
+      <span className="material-icons primary-color"><FaRegUser /></span>
+    </button>
   </nav>
 );
 
